a131ebff2cf722b48046301e9f48ef95c0c060b1
volpino <fox91@anche.no>
2013-02-06 11:11:46 +0100

    cleaning

--- setup.py
+++ setup.py
@@ -1,31 +1,28 @@
 from setuptools import setup, find_packages
-import sys, os
 
 version = '0.1'
 
 setup(
-	name='ckanext-patstatweb',
-	version=version,
-	description="StatWeb PAT",
-	long_description="""\
-	""",
-	classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-	keywords='',
-	author='dev@spaziodati.eu',
-	author_email='dev@spaziodati.eu',
-	url='',
-	license='WTFPL',
-	packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-	namespace_packages=['ckanext', 'ckanext.patstatweb'],
-	include_package_data=True,
-	zip_safe=False,
-	install_requires=[
-		# -*- Extra requirements: -*-
-	],
-	entry_points=\
-	"""
+    name='ckanext-patstatweb',
+    version=version,
+    description="StatWeb PAT",
+    long_description="",
+    classifiers=[],
+    keywords='',
+    author='dev@spaziodati.eu',
+    author_email='dev@spaziodati.eu',
+    url='',
+    license='WTFPL',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    namespace_packages=['ckanext', 'ckanext.patstatweb'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        # -*- Extra requirements: -*-
+    ],
+    entry_points="""
         [ckan.plugins]
-	# Add plugins here, eg
-	patstatweb=ckanext.patstatweb.harvesters:PatStatWebHarvester
-	""",
+    # Add plugins here, eg
+    patstatweb=ckanext.patstatweb.harvesters:PatStatWebHarvester
+    """,
 )
