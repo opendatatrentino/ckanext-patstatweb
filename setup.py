c5fd18486da9eb98c0fdddd6bca56d3848d0a362
volpino <fox91@anche.no>
2013-05-15 09:52:23 +0200

    adding requests to requirements

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        # -*- Extra requirements: -*-
+        'requests'
     ],
     entry_points="""
     [ckan.plugins]
